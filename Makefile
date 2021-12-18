00e34970e8a08baf559da572ad813048dbe23c3a
Pavel N_k <pav.n95@gmail.com>
2021-12-18 19:33:43 +0300

    rename Diff-action.yml to diff-check.yml and fix problems

--- Makefile
+++ Makefile
@@ -17,6 +17,6 @@ test:
 	npm test
 
 test-coverage:
-	npm test --coverage --coverageProvider=v8
+	npm test -- --coverage --coverageProvider=v8
 
 .PHONY: test
--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -2,7 +2,7 @@
 import * as fs from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import { expect, test } from '@jest/globals';
+import { expect, test, beforeAll } from '@jest/globals';
 import genDiff from '../src/diff.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -11,8 +11,13 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+let result;
+
+beforeAll(() => {
+  result = readFile('expected_result.txt');
+});
+
 test('genDiff', () => {
-  const result = readFile('expected_result.txt');
   const diff1 = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
   expect(diff1).toEqual(result);
 });
